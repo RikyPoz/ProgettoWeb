fd7f8ed706eba3d137f5e2c6a411a57e2be583aa
Pietro Pasini <pietro.pasini4@studio.unibo.it>
2025-01-13 22:02:23 +0100

    login dal carrello

--- Aeki/shoppingCart.php
+++ Aeki/shoppingCart.php
@@ -1,17 +1,15 @@
 <?php
 require_once 'bootstrap.php';
 
-/*if (!isset($_SESSION['user_id'])) {
-    // Reindirizza alla pagina di login
+if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
-}*/
+}
 
 $templateParams["titolo"] = "Aeki - carrello";
 $templateParams["nome"] = "shoppingCart_main.php";
 $_SESSION['user_id'] = "user1";
 $nomeUtente = $_SESSION['user_id'];
-$nomeUtente = "user1";
 $templateParams["prodotti"] = $dbh->getCarrello($nomeUtente);
 $templateParams["js"] = array("js/shoppingCart.js");
 
